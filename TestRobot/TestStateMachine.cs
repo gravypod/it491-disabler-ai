14446848df39d04f25404a75a764afea6213d563
Joshua Katz <gravypod@gravypod.com>
2019-09-29 23:00:22 -0400

    Add unit tests for RobotAiState Disabled.
    Check to see if headshots or negative health result in Disabled.

--- TestRobot/TestStateMachine.cs
+++ TestRobot/TestStateMachine.cs
@@ -491,5 +491,53 @@ namespace TestRobot
             ai.State = RobotAiState.AlertReposition;
             Assert.False(ai.Can(RobotAiState.AlertAttack));
         }
+        
+        
+        // Disabled Unit Tests
+        [Test]
+        public void TestNoHealthMoveToDisabled()
+        {
+            RobotAi ai = new MockRobotAi();
+            MockRobot robot = (MockRobot) ai.Robot;
+
+            robot.Health = 0;
+            ai.State = RobotAiState.Patrol;
+            Assert.True(ai.Can(RobotAiState.Disabled));
+        }
+        
+        [Test]
+        public void TestNegativeHealthMoveToDisabled()
+        {
+            RobotAi ai = new MockRobotAi();
+            MockRobot robot = (MockRobot) ai.Robot;
+
+            robot.Health = -1;
+            ai.State = RobotAiState.Patrol;
+            Assert.True(ai.Can(RobotAiState.Disabled));
+        }
+        
+        [Test]
+        public void TestHeadShotMoveToDisabled()
+        {
+            RobotAi ai = new MockRobotAi();
+            MockRobot robot = (MockRobot) ai.Robot;
+
+            robot.Head.Shot = true;
+            ai.State = RobotAiState.Patrol;
+            Assert.True(ai.Can(RobotAiState.Disabled));
+        }
+        
+                
+        [Test]
+        public void TestHealthyAndNoHeadShotShouldNotMoveToDisabled()
+        {
+            RobotAi ai = new MockRobotAi();
+            MockRobot robot = (MockRobot) ai.Robot;
+
+            robot.Head.Shot = false;
+            robot.Health = 100;
+            ai.State = RobotAiState.Patrol;
+            Assert.False(ai.Can(RobotAiState.Disabled));
+        }
     }
 }
